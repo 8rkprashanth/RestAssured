bf8bd60515d106dbb8673121f57bb4b91b50f234
Prashantha <8rkprashanth@gmail.com>
2022-04-05 20:07:01 +0530

    new changes

---
+++ test/src/Hello123.java
@@ -0,0 +1,12 @@
+public class Hello123 {
+    public static void main(String[] args) {
+         String name3= name("ram", "bim");
+        System.out.println("concatenated name is "+name3);  
+    }
+
+    public static String name(String a,String b) {
+        String c = a+b;
+        return c;
+        
+    }
+}
